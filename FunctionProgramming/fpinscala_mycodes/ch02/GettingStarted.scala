0a5b61005b35804d395ffcb75e9e3c1000dd8652
kzono <matsuzon@gmail.com>
2017-12-27 08:52:35 +0900

    add factorial()

--- FunctionProgramming/fpinscala_mycodes/ch02/GettingStarted.scala
+++ FunctionProgramming/fpinscala_mycodes/ch02/GettingStarted.scala
@@ -4,6 +4,14 @@
 
 
 object MyModule{
+	def factorial(n: Int): Int = {
+		@annotation.tailrec
+		def go(n: Int, acc: Int): Int =
+			if (n <= 0) acc
+			else go(n-1, n*acc)
+		go(n, 1)
+	}
+
 	def sum(n: Int): Int = {
 		@annotation.tailrec
 		def go(n: Int, acc: Int): Int =
@@ -11,21 +19,22 @@ object MyModule{
 			else go(n-1, n+acc)
 		go(n, 0)
 	}
+/*
 	private def formatSum(x: Int) = {
 		val msg = "The total sum value of %d is %d"
 		msg.format(x, sum(x))
 	}
-
+*/
 
 	def abs(n: Int): Int =
 		if (n < 0) -n
 		else n
-
+/*
 	private def formatAbs(x: Int) = {
 		val msg = "The absolute value of %d is %d"
 		msg.format(x, abs(x))
 	}
-
+*/
 	def formatResult(name: String, n: Int, f: Int => Int) = {
 		val msg = "The %s of %d is %d."
 		msg.format(name, n, f(n))
@@ -35,5 +44,6 @@ object MyModule{
 		//println(formatAbs(-321))
 		//println(formatSum(10))
 		println(formatResult("total sum", 10, sum))
+		println(formatResult("factorial", 5, factorial))
 
 }
